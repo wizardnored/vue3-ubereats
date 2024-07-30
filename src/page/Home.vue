249bad628544385b7c03a9091a12ce048044a339
wizardnored <antoinederon@gmail.com>
2024-07-30 16:24:42 +0200

    feat: add header + banner

--- src/page/Home.vue
+++ src/page/Home.vue
@@ -1,5 +1,10 @@
 <template>
   <div class="home">
+    <div class="header">
+      <img src="https://d3i4yxtzktqr9n.cloudfront.net/web-eats-v2/ee037401cb5d31b23cf780808ee4ec1f.svg" alt="" srcset="">
+      <input type="text" placeholder="De quoi avez vous envie?">
+    </div>
+    <div class="banner"></div>
     <RestaurantRow
       v-for="(data, i) in restaurants"
       :key="i"
@@ -58,4 +63,32 @@ export default {
 };
 </script>
 
-<style></style>
+<style lang="scss">
+.home{
+  .header{
+    height: 120px;
+    width:100%;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    img{
+      width: 200px;
+    }
+    input{
+      background-color: #f6f6f6;
+      border: none;
+      height: 60px;
+      width: 400px;
+      outline: none;
+      padding-left: 20px;
+    }
+  }
+  .banner{
+    height: 200px;
+    width: 100%;
+    background-image: url("https://techcrunch.com/wp-content/uploads/2019/04/uber-eats-icon-ios.jpg");
+    background-size: cover;
+    background-position: center center;
+  }
+}
+</style>
